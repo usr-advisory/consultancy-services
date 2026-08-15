10b5a091fbfd78c5a86eb037c0318853e5756442
usr-advisory <usrresearchfinancial@gmail.com>
2026-08-15 18:58:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menu=document.querySelector('.menu'),nav=document.querySelector('nav');if(menu)menu.onclick=()=>nav.classList.toggle('open');document.querySelectorAll('nav a').forEach(a=>a.onclick=()=>nav.classList.remove('open'));document.getElementById('year').textContent=new Date().getFullYear();
